247063374a380df2f7e5fa7665da7550e9c30b41
unknown <kingdnz333@gmail.com>
2026-03-20 14:45:57 -0300

    teste de solucao

--- back-end/server.js
+++ back-end/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.post('/send', async (req, res) => {
+app.post("/send", async (req, res) => {
   const { name, email, phone, message } = req.body;
 
   if (!name || !email || !phone || !message) {
